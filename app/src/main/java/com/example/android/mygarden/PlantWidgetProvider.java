a667e8da57404fda3460ce51d6180104fb57b692
WulanR <wulanr161299@gmail.com>
2017-12-29 02:01:18 +0700

    Create PlantWidgetProvider Class

---
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -0,0 +1,10 @@
+package com.example.android.mygarden;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Me on 12/29/2017.
+ */
+
+public class PlantWidgetProvider extends AppWidgetProvider {
+}
